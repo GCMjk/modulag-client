f3768b839e534bf3edfa63c30052479727203714
Martin <majok5040@gmail.com>
2023-03-09 18:08:54 -0600

    tailwind configuration

---
+++ src/components/But.tsx
@@ -0,0 +1,37 @@
+export default function Example() {
+    return (
+      <>
+        <button
+          type="button"
+          className="rounded bg-indigo-600 py-1 px-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Button text
+        </button>
+        <button
+          type="button"
+          className="rounded bg-indigo-600 py-1 px-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Button text
+        </button>
+        <button
+          type="button"
+          className="rounded-md bg-indigo-600 py-1.5 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Button text
+        </button>
+        <button
+          type="button"
+          className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Button text
+        </button>
+        <button
+          type="button"
+          className="rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Button text
+        </button>
+      </>
+    )
+  }
+  
